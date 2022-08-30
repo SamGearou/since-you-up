c7386ffb8eddb878b5e57087dd531d2123ac879f
samgearou <samgearou@robinhood.com>
2022-08-30 08:52:43 -0700

    Initial Commit

---
+++ since-you-up/since_you_upApp.swift
@@ -0,0 +1,17 @@
+//
+//  since_you_upApp.swift
+//  since-you-up
+//
+//  Created by Sam Gearou on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct since_you_upApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
