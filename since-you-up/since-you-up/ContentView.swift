a0479842e491bbef48deddee8eecbb1144a7db65
Sam Gearou <sam.gearou@robinhood.com>
2023-08-26 16:45:57 -0700

    initial login screen

--- since-you-up/since-you-up/ContentView.swift
+++ since-you-up/since-you-up/ContentView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Login")
+                .font(.title)
+            Form {
+                Section(header: Text("Username")) {
+                    TextField("", text: $username, prompt: Text("Required"))
+                }
+                Section(header: Text("Password")) {
+                    SecureField("", text: $password, prompt: Text("Required"))
+                }
+                Button {
+                    // TODO (Sam Gearou): save username
+                    // and password to a database
+                    // and switch to a different view
+                } label: {
+                    Text("Sign In")
+                }
+            }
+            .buttonStyle(.bordered)
         }
         .padding()
     }
